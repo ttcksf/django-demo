69308561aa07eafa54195538e10ac65aa398fb54
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-03 01:04:54 +0900

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+# https://www.djangoproject.com/download/
+# WINもしくはMACのコマンドを実行
+# python -m pip install Django==5.0.6
+# 仮想環境の構築
+# pip install pipenv
+# 仮想環境のアクティブ化
+# pipenv shell
+# 仮想環境にdjangoをインストール
+# pipenv install django
+# バージョンの確認
+# pip freeze
+# プロジェクトの作成
+# django-admin startproject myproject
+# フォルダに移動
+# cd myproject
+# 作業フォルダの作成
+# python manage.py startapp myapp
+# サーバーの起動
+# python manage.py runserver
